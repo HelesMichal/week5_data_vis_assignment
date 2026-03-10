adc04d79a7af7907f1bd0405f3135f7205dfe679
HelesMichal <michal.heles@student.howest.be>
2026-03-10 17:42:21 +0100

    modfication of posting species and birds

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from database import start_db
+from database import get_session
+from routers.species import router as species_router
+from routers.birds import router as birds_router
 
 app = FastAPI()
+app.include_router(species_router)
+app.include_router(birds_router)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
---
+++ repositories/birds.py
@@ -0,0 +1,28 @@
+from sqlmodel import Session, select
+from models.birds import Bird, BirdCreate
+from models.species import Species
+from fastapi import HTTPException
+
+class BirdRepository:
+    def __init__(self, session: Session):
+        self.session = session
+    
+    def get_all(self):
+        statement = select(Bird)
+        items = self.session.exec(statement).all()
+        return items
+
+    def insert(self, payload: BirdCreate):
+        # Check if species exists
+        species = self.session.get(Species, payload.species_id)
+        if not species:
+            raise HTTPException(
+                status_code=404,
+                detail=f"Species with id {payload.species_id} does not exist"
+            )
+        
+        item = Bird.model_validate(payload)
+        self.session.add(item)
+        self.session.commit()
+        self.session.refresh(item)
+        return item
